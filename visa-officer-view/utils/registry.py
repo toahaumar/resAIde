e3f4260b8a77f86f51a197c715ffe1391c4e90c2
hash-ir <hashirahmad@zhcet.ac.in>
2025-03-16 03:13:22 +0100

    chore(visa-officer-view): move personal info registry to utils

---
+++ visa-officer-view/utils/registry.py
@@ -0,0 +1,17 @@
+PERSONAL_INFO_REGISTRY = {
+    "John Doe": {
+        "date_of_birth": "1990-05-15",
+        "nationality": "USA",
+        "passport_number": "AB123456",
+    },
+    "Maria Garcia": {
+        "date_of_birth": "1988-03-21",
+        "nationality": "Spain",
+        "passport_number": "XY789012",
+    },
+    "Deepti Singhal": {
+        "date_of_birth": "1981-06-02",
+        "nationality": "Indian",
+        "passport_number": "L9492297",
+    }
+}
